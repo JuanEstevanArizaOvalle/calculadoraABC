8216981f1f099ed820b5d265703e150133473ec5
USUARIO <caballero.heiner@iemodeliaibague.edu.co>
2024-07-23 16:19:47 -0500

    resta de caballero

--- src/resta.java
+++ src/resta.java
@@ -1,8 +1,19 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.swing.*;
+import java.util.Scanner;
+
 public class resta {
+
     public static void main (String [] args){
-          int resta;
-
-
+        Scanner entrada = new Scanner(System.in);
+        int resta;
+     System.out.println("porfavor ingrese el numero 1");
+     int n1 = entrada.nextInt();
+        System.out.println("porfavor ingrese el numero 2");
+        int n2 = entrada.nextInt();
+        resta = n1 - n2;
+        System.out.println("señor usuario su resultado es: " + resta);
 
 
 
