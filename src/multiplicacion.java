3506fc45087f12bdbb84c7095073af3daefcbc62
yesicadayana12 <dayanayesica226@gmail.com>
2024-07-23 16:16:41 -0500

    multiplicacion yess

--- src/multiplicacion.java
+++ src/multiplicacion.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class multiplicacion {
 
+    public static void main (String [] args){
+        Scanner entrada = new Scanner(System.in);
+
+        int multiplicacion;
+
+        System.out.println("por favor ingrese el primer numero");
+        int num1 = entrada.nextInt();
+        System.out.println("por favor ingrese el segundo numero");
+        int num2 = entrada.nextInt();
+
+        multiplicacion = num1*num2;
+
+        System.out.println("Señor usuario el resultado de su multplicacion es :"  + multiplicacion);
+
+    }
 }
